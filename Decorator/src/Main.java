308d7868bb628bf66c64ee7bc490c9e2b5947c6e
Amalia Hajarani <lalamalia2103@gmail.com>
2020-07-10 15:55:48 +0700

    Upload Decorator

---
+++ Decorator/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        room myBedRoom = new bedRoom(10);
+        room myLivingRoom = new livingRoom(18);
+
+        room myBedRoomNewDesign = new design1(myBedRoom);
+        room myLivingRoomNewDesign = new design1(myLivingRoom);
+
+        myBedRoom.printDescription();
+        myBedRoomNewDesign.printDescription();
+        myLivingRoom.printDescription();
+        myLivingRoomNewDesign.printDescription();
+    }
+}
